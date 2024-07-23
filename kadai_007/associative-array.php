09467088f74c024c47a11117239dd9dc266a6242
shimanosakuya <160805776+shimano-sakuya@users.noreply.github.com>
2024-07-23 09:36:48 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+<p>
+  <?php
+  $associative_array = ['name' => 'onion' ,'price' => 200 ,'weight' => 160];
+
+  print_r($associative_array);
+  ?>
+</p>
+<body>
+  
+</body>
+</html>
